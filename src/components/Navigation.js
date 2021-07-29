90cfbd9c761129a77dddb0ba73ae5d104cc638ac
Himanshu Rana <hranaritsh@gmail.com>
2021-07-29 17:48:20 +0530

    navigation-created

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
-import {useNavigate} from "react-router-dom"
+import {Link,useNavigate} from "react-router-dom"
+import { SideBarData } from '../utils/SideBarData';
  const Navigation = () => {
      const navigate = useNavigate();
+     const [sideBar,setSideBar] = useState(false);
+     const showSideBar = ()=>setSideBar(!sideBar);
 
     return (
        <>
-        <div className="header">
+            <div className="header">
                 <div>
-                   <MenuOutlinedIcon/>
+                <Link to="#">
+                    <MenuOutlinedIcon onClick={showSideBar} />
+                </Link>
+                   
                 </div>
                 <div style={{width:"30px",height:"30px",backgroundColor:"black",borderRadius:"500%"}} >
                     
@@ -18,6 +24,27 @@ import {useNavigate} from "react-router-dom"
                     <ShoppingCartOutlinedIcon onClick={()=>navigate("/home/cart")} />
                 </div>
             </div>
+            <nav className={sideBar ? "nav-menu active" : "nav-menu"}>
+                <ul onClick={showSideBar} className="nav-menu-items">
+                    <li className="navbar-toggle">
+                        <Link className="menu-bars" to="#">
+                            Close
+                        </Link>
+                    </li>
+                    {
+                        SideBarData.map((item,index)=>
+                        {
+                            return (
+                                <li key={index} className={item.cName} >
+                                    <Link to={item.path}>
+                                        {item.title}
+                                    </Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
        </>
     )
 }
---
+++ src/utils/SideBarData.js
@@ -0,0 +1,18 @@
+
+export const SideBarData = [
+    {
+        title:"Home",
+        path:"/home",
+        cName:"nav-text"
+    },
+    {
+        title:"Products",
+        path:"/home/product",
+        cName:"nav-text"
+    },
+    {
+        title:"Shop",
+        path:"/home/shop",
+        cName:"nav-text"
+    }
+]
